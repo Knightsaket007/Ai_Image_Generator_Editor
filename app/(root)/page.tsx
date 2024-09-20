1b6cf93d87245281a9af44d3ac5eb168409bddad
Knightsaket007 <109225097+Knightsaket007@users.noreply.github.com>
2024-09-20 15:36:44 +0530

    third change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,6 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
-import { FC } from 'react'
-export const Home:FC = () => {
+const Home = () => {
   return (
     <>
     <p>home</p>
